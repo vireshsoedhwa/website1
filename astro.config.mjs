23065a5f5bf24fee9078858c3d368bc628c4865a
Viresh Soedhwa <vireshsoedhwa@gmail.com>
2026-05-11 20:43:41 -0700

    Add Docker support, update Astro config, create portfolio components and styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://viresh.github.io',
-  base: '/website1',
   output: 'static',
+  server: { host: '0.0.0.0' },
 });
